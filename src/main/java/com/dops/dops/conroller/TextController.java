2bcd38db4934d5e7c30c6be6f5e88d972f3d0383
Denis Kalinin <k-d-s@mail.ru>
2021-02-03 23:17:31 +0300

    add log

--- src/main/java/com/dops/dops/conroller/TextController.java
+++ src/main/java/com/dops/dops/conroller/TextController.java
@@ -2,6 +2,8 @@ package com.dops.dops.conroller;
 
 import com.dops.dops.dao.City;
 import com.dops.dops.dao.CityRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,7 @@ import java.util.stream.Collectors;
 @Controller
 public class TextController {
 
+    Logger logger = LoggerFactory.getLogger(TextController.class);
     final   CityRepository repository;
 
     public TextController(CityRepository repository) {
@@ -29,8 +32,9 @@ public class TextController {
             repository.save(new City("Магадан"));
             repository.save(new City("Сочи"));
             repository.save(new City("Минск"));
+            logger.info("Add city for empty repository.");
         }
-        all.forEach(System.out::println);
+        all.forEach(e -> logger.info(e.toString()));
         return "<h3>Main page!</h3>";
     }
 
@@ -38,6 +42,7 @@ public class TextController {
     public @ResponseBody String listCity(@RequestParam(value = "name", required = false) String name) {
         if(name != null && !name.isEmpty()) repository.save(new City(name));
         List<City> all = repository.findAll();
+        logger.info("Get all city.");
         return all.stream().map(e -> "<h3>" + e.toString() + "</h3>")
                 .collect(Collectors.joining("\n"));
     }
@@ -45,6 +50,7 @@ public class TextController {
     @GetMapping("/city/{id}")
     public @ResponseBody String getCity(@PathVariable Long id) {
         Optional<City> one = repository.findById(id);
+        logger.info("Get by id");
         return one.map(city -> "<h3>" + city.toString() + "</h3>")
                 .orElse("<h3>Не найдено!!</h3>");
     }
@@ -52,6 +58,7 @@ public class TextController {
     @GetMapping("/delete/{id}")
     public RedirectView deleteCity(@PathVariable Long id) {
         repository.deleteById(id);
+        logger.info("Delete by id");
         return new RedirectView("/city");
     }
 }
